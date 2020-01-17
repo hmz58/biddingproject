a080376ff52941b48320b3e68739c6f3a67b3717
hmz58 <46661139+hmz58@users.noreply.github.com>
2020-01-18 01:55:13 +0530

    Add files via upload

--- loginregister/main.py
+++ loginregister/main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.secret_key = 'your secret key'
 
 # Enter your database connection details below
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'hosting@12345'
 app.config['MYSQL_DB'] = 'biddingsystem'
